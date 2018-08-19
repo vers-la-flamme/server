050d92a66a0638c75d713dad6cf517dc3a0f997c
Peter Rudenko <peter.rudenko@gmail.com>
2018-08-19 10:09:33 +0400

    Clean up some unused stuff

--- apps/api/lib/api_web/router.ex
+++ apps/api/lib/api_web/router.ex
@@ -1,26 +1,21 @@
 defmodule ApiWeb.Router do
   use ApiWeb, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
+    # plug Api.Json.CamelCaseDecoder
   end
+
+  # pipeline :authenticated do
+  #   plug Api.Auth.CheckToken
+  # end
 
   scope "/", ApiWeb do
-    pipe_through :browser # Use the default browser stack
-
-    get "/", PageController, :index
+    pipe_through :api
+    # post "/join", RegistrationController, :sign_up, as: :signup
+    # post "/login", SessionController, :sign_in, as: :login
+    # post "/reauth", SessionController, :refresh_token, as: :relogin
+    # get "/session-status", SessionController, :is_authenticated, as: :session_status
+    # get "/me", UserController, :get_current_user, as: :user
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", ApiWeb do
-  #   pipe_through :api
-  # end
 end
