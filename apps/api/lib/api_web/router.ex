8ffe1db2c59c40a94e9446f0add92f868fc3d5fc
Peter Rudenko <peter.rudenko@gmail.com>
2018-08-19 14:04:41 +0400

    Set up dependencies and routes

--- apps/api/lib/api_web/router.ex
+++ apps/api/lib/api_web/router.ex
@@ -3,19 +3,23 @@ defmodule ApiWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
-    # plug Api.Json.CamelCaseDecoder
+    plug Api.Json.CamelCaseDecoder
   end
 
-  # pipeline :authenticated do
-  #   plug Api.Auth.CheckToken
-  # end
+  pipeline :authenticated do
+    plug Api.Auth.CheckToken
+  end
 
-  scope "/", ApiWeb do
+  scope "/v1", ApiWeb, as: :api_v1 do
     pipe_through :api
-    # post "/join", RegistrationController, :sign_up, as: :signup
-    # post "/login", SessionController, :sign_in, as: :login
-    # post "/reauth", SessionController, :refresh_token, as: :relogin
-    # get "/session-status", SessionController, :is_authenticated, as: :session_status
-    # get "/me", UserController, :get_current_user, as: :user
+    post "/join", RegistrationController, :sign_up, as: :signup
+    post "/login", SessionController, :sign_in, as: :login
+
+    pipe_through :authenticated
+    post "/reauth", SessionController, :refresh_token, as: :relogin
+    get "/session-status", SessionController, :is_authenticated, as: :session_status
+
+    get "/me", UserController, :get_current_user, as: :user
+    resources "/users", UserController, only: [:index, :delete], as: :user
   end
 end
