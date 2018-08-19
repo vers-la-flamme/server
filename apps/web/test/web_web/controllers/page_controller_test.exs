166bf2b65096cb59c0308469d276e9a424db4127
Peter Rudenko <peter.rudenko@gmail.com>
2018-08-19 10:30:50 +0400

    Replace phoenix welcome page with custom stub

--- apps/web/test/web_web/controllers/page_controller_test.exs
+++ apps/web/test/web_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule WebWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Vers la flamme"
   end
 end
